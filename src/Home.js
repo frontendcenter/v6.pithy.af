9dbc18919ad9961df4cc6062c45870488cc6e1c0
Glen Maddern <glenmaddern@gmail.com>
2017-08-22 13:22:33 +1000

    this is not all that good

--- src/Home.js
+++ src/Home.js
@@ -4,27 +4,32 @@ import End from './End'
 import { observer } from 'mobx-react'
 import state, { fetchQuotes } from './state'
 
-class Home extends React.Component {
+const Await = class extends React.Component {
   componentWillMount() {
-    fetchQuotes()
+    if (this.props.invoke) this.props.invoke()
   }
 
   render() {
-    const { quotes } = state
-    return (
-      <div className="Quotes">
-        {
-          quotes && quotes
-            .map((quote, i) => <Quote key={i} quote={quote}/>)
-            .concat(
-              <End key="end">
-                <span>That's all the quotes we have.</span>
-              </End>
-            )
-        }
-      </div>
-    )
+    if (this.props.data()) {
+      return <div>{ this.props.children }</div>
+    } else {
+      return null
+    }
   }
 }
 
-export default observer(Home)
+export default observer(() => (
+  <div className="Quotes">
+    <Await data={ () => state.quotes } invoke={ fetchQuotes }>
+      {
+        state.quotes && state.quotes
+          .map((quote, i) => <Quote key={i} quote={quote}/>)
+          .concat(
+            <End key="end">
+              <span>That's all the quotes we have.</span>
+            </End>
+          )
+      }
+    </Await>
+  </div>
+))
