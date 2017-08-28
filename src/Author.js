d2dc986d3cc7f95e125ca2010bc95a4a6c3c7cdc
Glen Maddern <glenmaddern@gmail.com>
2017-08-28 17:18:41 +1000

    this api actually looks pretty sweet

--- src/Author.js
+++ src/Author.js
@@ -5,30 +5,24 @@ import Quote from './Quote'
 import End from './End'
 import state, { fetchQuotesForAuthor } from './state'
 
-export const Author = observer(class extends React.Component {
-  componentWillMount() {
-    fetchQuotesForAuthor(this.props.id)
-  }
-
-  render() {
-    const quotes = state.quotes_by_author.get(this.props.id)
-    return (
-      <div className="Quotes">
-        { this.props.exclude && <div/> }
-        {
-          quotes && quotes.map((quote, i) =>
-            this.props.exclude === quote.id
-              ? null
-              : <Quote key={i} quote={quote}/>
-          ).concat(
-            <End key="end">
-              <span>No more quotes for <em>{quotes[0].name}</em>.</span>
-            </End>
-          )
-        }
-      </div>
-    )
-  }
+export const Author = observer(({ id, exclude }) => {
+  const quotes = state.quotes_by_author.get(id)
+  return (
+    <div className="Quotes">
+      {exclude && <div/>}
+      {
+        fetchQuotesForAuthor(id) && quotes.map((quote, i) =>
+          exclude === quote.id
+            ? null
+            : <Quote key={i} quote={quote}/>
+        ).concat(
+          <End key="end">
+            <span>No more quotes for <em>{quotes[0].name}</em>.</span>
+          </End>
+        )
+      }
+    </div>
+  )
 })
 
 export default ({ match: { params } }) => <Author id={params.id}/>
--- src/state.js
+++ src/state.js
@@ -7,35 +7,23 @@ const state = observable({
   quotes_by_author: new Map(),
   error: false
 })
+const api_requests = observable(new Map())
 
 export default state
 
-const snapshot_fetch = url => snapshot(() =>
+const _snapshot_fetch = url => snapshot(() =>
   fetch(url).then(response => response.json())
 )
 
-export const fetchQuotes = (() => {
-  const status = observable({
-    loading: false,
-    loaded: false
-  })
-  return () => {
-    if (status.loading) return status.loaded
-    status.loading = true
-    console.log("Fetching quotes")
-    snapshot_fetch(`${API}/quotes`)
-      .then(quotes => {
-        state.quotes = quotes
-      }, () => state.error = true)
-      .then(() => status.loaded = true)
-    return status.loaded
+const snapshot_fetch = (url, success) => {
+  if (!api_requests.has(url)) {
+    api_requests.set(url, false)
+    _snapshot_fetch(url)
+      .then(success, () => state.error = true)
+      .then(() => api_requests.set(url, true))
   }
-})()
+  return api_requests.get(url)
+}
 
-export const fetchQuotesForAuthor = (id) => (
-  snapshot_fetch(`${API}/authors/${id}`)
-    .then(quotes => {
-      state.quotes_by_author.set(id, quotes)
-      console.log(toJS(state))
-    }, () => state.error = true)
-)
+export const fetchQuotes = () => snapshot_fetch(`${API}/quotes`, quotes => state.quotes = quotes)
+export const fetchQuotesForAuthor = id => snapshot_fetch(`${API}/authors/${id}`, quotes => state.quotes_by_author.set(id, quotes))
